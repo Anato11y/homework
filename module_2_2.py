789d215a147e40004a60dfa948583b8f5e7a3284
Anato11y <tol66@yandex.ru>
2024-06-14 19:37:10 +0800

    module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second=input('Введите число second: ')
 third=input('Введите число third: ')
 if first==second and first==third:
     print('Количество одинаковых чисел:',3)
-elif first==second or first==third or first==third:
+elif first==second or first==third or second==third:
     print('Количество одинаковых чисел:',2)
 else:
     print('Количество одинаковых чисел:',0)
